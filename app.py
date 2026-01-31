4f77be7bda53b2cb3b233f21d33622dd8c2ee940
Lalitha Sharma Velpuru <lalithasharmavelpuru@Lalithas-MacBook-Air.local>
2026-01-31 19:46:20 +0530

    last commit

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Streamlit App", layout="wide")
+
+st.title("🚦 Intelligent Traffic Monitoring System")
+st.write("Welcome to the dashboard")
